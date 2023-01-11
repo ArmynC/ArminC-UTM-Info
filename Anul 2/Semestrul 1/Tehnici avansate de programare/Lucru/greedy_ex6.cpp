f30595a97d95cab6b6597cceaebd1e7fae9722d1
ArminC <arminandrey@gmail.com>
2023-01-12 01:11:28 +0200

    UTM

--- Anul 2/Semestrul 1/Tehnici avansate de programare/Lucru/greedy_ex6.cpp
+++ Anul 2/Semestrul 1/Tehnici avansate de programare/Lucru/greedy_ex6.cpp
@@ -8,37 +8,66 @@ mediu de asteptare a notelor.
 */
 
 #include <iostream>
-
 using namespace std;
 
+// structura pentru student
 struct student
 {
-    int id;
-    float t1,t2,t3;
-
+    int id; // id-ul studentului
+    float tema_1, tema_2, tema_3; // timpul de evaluare pentru fiecare tema
 };
-float timp_total(student a)
+
+// functie pentru a calcula timpul total de evaluare al unei teme
+float timp_total(student x)
 {
-    return a.t1+a.t2+a.t3;
+    return x.tema_1 + x.tema_2 + x.tema_3;
 }
 
 int main()
 {
     int  n, i, j;
-    student stud[100], aux;
+    student stud[100], aux; // vectorul de studenti si un element auxiliar pentru sortare
 
+    cout << "Introdu numarul de studenti: ";
     cin >> n;
 
-    for(i=1; i<=n; i++)
+    // Citirea datelor pentru fiecare student
+    for (i = 1; i <= n; i++)
     {
-        cin >> stud[i].id >> stud[i].t1 >> stud[i].t2 >> stud[i].t3;
+        cout << endl << "Introdu ID-ul studentului nr. " << i << ": ";
+        cin >> stud[i].id;
+        cout << "Introdu timpul de evaluare al temei nr. 1: ";
+        cin >> stud[i].tema_1;
+        cout << "Introdu timpul de evaluare al temei nr. 2: ";
+        cin >> stud[i].tema_2;
+        cout << "Introdu timpul de evaluare al temei nr. 3: ";
+        cin >> stud[i].tema_3;
     }
 
-    for(i=1; i<n; i++)
+    // ta: timpul total de asteptare pentru toti studentii
+    // tm: timpul mediu de asteptare
+    float ta = 0, tm = 0;
+
+    cout << endl;
+    cout << "Valori initiale:" << endl;
+    cout << "ID" << "\t\t" << "T1" << "\t" << "T2" << "\t" << "T3" << "\t" << "Timp total evaluare" << "\t" << "Timp asteptare persoana" << endl;
+    for (i = 1; i <= n; i++)
     {
-        for(j=i+1; j<=n; j++)
+        // se adauga la ta timpul total de evaluare pentru fiecare student
+        ta += timp_total(stud[i]);
+        // se adauga la tm timpul total de asteptare pentru fiecare student
+        tm += ta;
+        cout << stud[i].id << "\t\t" << stud[i].tema_1 << "\t" << stud[i].tema_2 << "\t" << stud[i].tema_3 << "\t" << timp_total(stud[i]) << "\t\t\t" << ta << endl;
+    }
+    cout << "Timpul mediu inital de asteptare: " << tm / n << endl;
+    cout << endl;
+
+    // sortare dupa timpul total de evaluare al fiecarui student
+    for (i = 1; i < n; i++)
+    {
+        for (j = i + 1; j <= n; j++)
         {
-            if(timp_total(stud[i])>timp_total(stud[j]))
+            if (timp_total(stud[i]) > timp_total(stud[j]))
             {
                 aux = stud[i];
                 stud[i] = stud[j];
@@ -46,11 +75,20 @@ int main()
             }
         }
     }
-
-    for(i=1; i<=n; i++)
+    ta = 0;
+    tm = 0;
+    cout << "Valori finale:" << endl;
+    cout << "ID" << "\t\t" << "T1" << "\t" << "T2" << "\t" << "T3" << "\t" << "Timp total evaluare" << "\t" << "Timp asteptare persoana" << endl;
+    for (i = 1; i <= n; i++)
     {
-
-        cout << stud[i].id << " " << stud[i].t1 << " " << stud[i].t2 << " " << stud[i].t3 <<" "<<timp_total(stud[i]) << endl;
+        // se adauga la ta timpul total de evaluare pentru fiecare student
+        ta += timp_total(stud[i]);
+        // se adauga la tm timpul total de asteptare pentru fiecare student
+        tm += ta;
+        cout << stud[i].id << "\t\t" << stud[i].tema_1 << "\t" << stud[i].tema_2 << "\t" << stud[i].tema_3 << "\t" << timp_total(stud[i]) << "\t\t\t" << ta << endl;
     }
+    cout << "Timpul mediu optim de asteptare: " << tm / n << endl;
 
+    return 0;
 }
+
